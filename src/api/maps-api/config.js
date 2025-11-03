a7282ab91d6f156316a305344998aa42e0df1e56
Dakshesh <dpasala@calpoly.edu>
2025-11-03 13:21:27 -0800

    Issue1 had an exposing API key... Sorry about that. This can be merged into master now

--- src/api/maps-api/config.js
+++ src/api/maps-api/config.js
@@ -1,2 +1,5 @@
-export const GOOGLE_MAPS_API_KEY = "AIzaSyDMHEuBuy9EgPEKIJL6eeabzKQfwAsMgD8";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
 export const BASE_URL = "https://maps.googleapis.com/maps/api";

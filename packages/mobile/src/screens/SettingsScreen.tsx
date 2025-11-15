a554e1ea8eb2dda4d5e243b54e71e56113103ede
yennyyma <mayennyy@gmail.com>
2025-11-14 18:08:12 -0800

    Ensured Xcode builds properly

--- packages/mobile/src/screens/SettingsScreen.tsx
+++ packages/mobile/src/screens/SettingsScreen.tsx
@@ -12,10 +12,7 @@ export default function SettingsScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Settings Screen</Text>
-      <Button
-        title="Go to Profile"
-        onPress={() => navigation.navigate('Home')}
-      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
     </View>
   );
 }

90ce1dbc2071338d9f12aebdd211a5739f4ffc62
KevinBeltran23 <kevinbeltran2437@gmail.com>
2026-01-30 12:50:51 -0800

    moved location services

--- packages/mobile/src/services/location/geofencing.ts
+++ packages/mobile/src/services/location/geofencing.ts
@@ -1,10 +1,10 @@
-import BackgroundGeolocation from "react-native-background-geolocation";
-import { OFFICE_LOCATIONS } from "../config/officeLocations";
+import BackgroundGeolocation from 'react-native-background-geolocation';
+import { OFFICE_LOCATIONS } from '../../config/officeLocations';
 
 export function registerOfficeGeofences() {
   BackgroundGeolocation.ready({});
 
-  OFFICE_LOCATIONS.forEach((office) => {
+  OFFICE_LOCATIONS.forEach(office => {
     BackgroundGeolocation.addGeofence({
       identifier: `office-${office.id}`,
       radius: office.radius_m,

3b9ad54ff6613dd3c228cf2ec8e63c2269da7779
KevinBeltran23 <kevinbeltran2437@gmail.com>
2026-02-01 14:38:43 -0800

    deleted fluff

--- packages/mobile/src/screens/admin/index.ts
+++
@@ -1,5 +0,0 @@
-// Admin screens - export barrel file
-export { default as AdminDashboardScreen } from './AdminDashboardScreen';
-export { default as AdminUsersScreen } from './AdminUsersScreen';
-export { default as AdminParkingScreen } from './AdminParkingScreen';
-export { default as AdminAlertsScreen } from './AdminAlertsScreen';

be11b3a7efa3b0f211cb92841b5d4cadab620c3a
Dakshesh <dpasala@calpoly.edu>
2026-02-01 20:44:19 -0800

    FINALLY FOR THE LOVE OF GOD IT WORKED

--- packages/mobile/src/navigation/AppNavigator.tsx
+++ packages/mobile/src/navigation/AppNavigator.tsx
@@ -1,3 +1,5 @@
+import 'react-native-reanimated';
+import 'react-native-gesture-handler';
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import type { RootStackParamList } from './types';
